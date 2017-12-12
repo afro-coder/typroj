589ab40cc371b5caef18df70d981ebdfacf37920
leon9923 <leon9923@gmail.com>
2017-12-12 20:40:57 +0530

    demo

--- FMSapp/Modules/Admin/views.py
+++ FMSapp/Modules/Admin/views.py
@@ -4,8 +4,8 @@ from flask_admin.contrib.sqla import ModelView
 from FMSapp import adminpg,db
 from flask import request,render_template,url_for,redirect,flash
 
-#@admin.route('/',methods=['GET','POST'])
-#def admin_default():
-#    return render_template("admin/base.html")
+@admin.route('/',methods=['GET','POST'])
+def admin_default():
+    return render_template("admin/base.html")
 adminpg.add_view(ModelView(User,db.session))
 adminpg.add_view(ModelView(Organization,db.session))
